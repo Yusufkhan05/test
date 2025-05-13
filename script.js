dbf07376817512e14bcb83851c92c4a2b0e1c576
Yusufkhan <yusufxonjalolxojayev95@gmail.com>
2025-05-13 23:52:58 +0500

    Testni yuklayapman

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("Script loaded");
+const button = document.getElementById("button");
+const input = document.getElementById("input");
+const output = document.getElementById("output");
+const error = document.getElementById("error");
+const loading = document.getElementById("loading");
+// const apiKey
